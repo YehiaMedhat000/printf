f29ee3c93a145d62611c83df55d73a0b706688f0
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2023-11-13 23:53:52 +0200

    Edited _printf.c

--- _printf.c
+++ _printf.c
@@ -11,13 +11,7 @@ int _printf(const char *format, ...)
 	va_list(args);
 	int i = 0, len = 0, format_len = 0;
 
-	func_f ff[] = {
-		{'c', prt_char},
-		{'s', prt_str},
-		{'i', prt_int},
-		{'d', prt_int},
-		{'\0', NULL}
-	};
+	func_f ff[] = {{'c', prt_char},{'s', prt_str},{'i', prt_int},{'d', prt_int},{'\0', NULL}};
 
 	va_start(args, format);
 
